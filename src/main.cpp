035c7dd6fdc4e13f696a57f235d5316c6a12802c
Dave Kosak <41718159+TheFargo@users.noreply.github.com>
2026-07-11 13:12:22 -0700

    Updated comments to reflect license

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 ﻿// ###########################################################################
 //                                  BLOSSOM
-//                         Programmable Light Display
+//                 Internet-Enabled Programmable Light Display
 //                         A Dave "Fargo" Kosak Joint
 //                                July,  2026
 //       AI Models Consulted: Gemini 3.5 Thinking, Claude Sonnet 4.5-4.6
+// ###########################################################################
+//                  Copyright 2026 Word Smith & Wright, LLC
+//  Project: Blossom - Internet-Enabled Programmable Light Display
+//  Description: Firmware for Raspberry Pi Pico 2W local wifi LED controller
+//  Author: Dave Kosak <dave@wordsmithandwright.com>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 // ###########################################################################
 
 #include <Arduino.h>
